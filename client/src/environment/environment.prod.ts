4af97d5e363552151bf7c53e2d9ec2e84f8767e1
unknown <dellavarghese2019@gmail.com>
2025-11-14 18:28:14 +0530

    Modified environment

--- client/src/environment/environment.prod.ts
+++ client/src/environment/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-domain.com/api'
+  authUrl: 'https://task-manager-app-ex77.onrender.com/api/auth',
+  dashboardUrl:'https://task-manager-app-ex77.onrender.com/api/dashboard',
+  taskUrl:'https://task-manager-app-ex77.onrender.com/api/tasks' ,
+  profileUrl:'https://task-manager-app-ex77.onrender.com/api/profile',
+  forgotApi:'https://task-manager-app-ex77.onrender.com/api/auth',
+  resetApi:'https://task-manager-app-ex77.onrender.com/api/auth'
 };
